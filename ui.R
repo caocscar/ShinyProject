8366688dc4503e9061d9a325280949bc62440fa6
unknown <caoa@CSCAR-2015-VFC.adsroot.itcs.umich.edu>
2017-05-05 15:09:48 -0400

    Changed side panel text

--- ui.R
+++ ui.R
@@ -7,10 +7,14 @@ shinyUI(fluidPage(
     titlePanel("Modeling Saving Ratio from Disposable Income"),
     sidebarLayout(
         sidebarPanel(
-            h3("Model"),
+            h3("Instructions"),
+            p("1. Select a box of points you want to model"),
+            p("2. Select which covariates you want to include"),
+            p("3. Repeat Steps 1 and/or 2 as req'd (formula will update after every step)."),
+            h4("Model"),
             textOutput("Mdl"),
             checkboxInput("pop","Include population covariates", value=FALSE),
-            checkboxInput("ddpi","Include ddpi covariate (% growth rate of disposable income)", value=FALSE)
+            checkboxInput("ddpi","Include ddpi (% growth rate of disposable income) covariate", value=FALSE)
         ),
         mainPanel(
             plotOutput("plot1", brush=brushOpts(id="brush1"))
